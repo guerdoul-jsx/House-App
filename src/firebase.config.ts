88ee17ee71ddce296c8cfc2b883fc776ee9cf893
Mahmoud <mahmoudguedoul0@gmail.com>
2023-06-15 16:49:49 +0100

    updated the firebase config

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAqyXDxaJz_SjbHoyp3Nwu5I_91LEwqI_0",
-  authDomain: "house-app-marketplace.firebaseapp.com",
-  projectId: "house-app-marketplace",
-  storageBucket: "house-app-marketplace.appspot.com",
-  messagingSenderId: "1096581317121",
-  appId: "1:1096581317121:web:6d86d36efb2db1a57b3600",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOAMIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
